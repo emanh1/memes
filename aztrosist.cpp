6417796095d2404fceea1761bd17d39785c9ebac
EmanhMC <emanhmc@gmail.com>
2017-12-27 16:11:08 +0700

    Create aztrosist.cpp

---
+++ aztrosist.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int numberArray[10][10];
+
+for(int i=1;i<11;i++){
+for(int j=1;j<11;j++){
+numberArray[i - 1][j - 1] = i*j;
+cout << numberArray[i-1][j-1] << "\t " << flush;
+}
+cout << endl;
+}
+return 0;
+}
